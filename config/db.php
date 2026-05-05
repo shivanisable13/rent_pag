d59b9735c8f5d2ca8f13158b34a99ec7b23cf21f
shivanisable13 <shivanisable075@gmail.com>
2026-05-05 13:06:28 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = mysqli_connect("localhost","root","","pg_rental");
+$conn = mysqli_connect("pg-db","root","root","pg_rental");
 
 if(!$conn){
     die("Connection Failed: " . mysqli_connect_error());
